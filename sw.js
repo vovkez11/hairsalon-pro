01de0d1480ef314036f310467431dc5f8c37d540
vladimir romanovski <vovkez11@gmail.com>
2025-10-14 11:38:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
-// Service Worker for Stylebook Pro - GitHub Pages
+// Service Worker for Stylebook Pro
 const CACHE_NAME = 'stylebook-pro-v1';
 const urlsToCache = [
   '/stylebook-pro/',
   '/stylebook-pro/index.html',
   '/stylebook-pro/manifest.json',
   '/stylebook-pro/assets/icons/icon-192.png',
-  '/stylebook-pro/assets/icons/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -18,7 +16,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
